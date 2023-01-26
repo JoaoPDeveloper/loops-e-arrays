9ed587f2817b7c3d46efb477a780027673202389
João Pedro Lima <joao_rubronegro@hotmail.com>
2023-01-26 20:37:58 -0300

    fork

--- src/br/com/dio/exercicios/arrays/Ex3_NumerosAleatorios.java
+++ src/br/com/dio/exercicios/arrays/Ex3_NumerosAleatorios.java
@@ -8,33 +8,34 @@ Ao final mostre os números e seus sucessores.
 */
 public class Ex3_NumerosAleatorios {
     public static void main(String[] args) {
-        Random random = new Random();
+        // Random random = new Random();
 
-        int[] numerosAleatorios = new int[20];
+        // int[] numerosAleatorios = new int[20];
 
-        for(int i = 0; i < numerosAleatorios.length; i++) {
-            int numero = random.nextInt(100);
-            numerosAleatorios[i] = numero;
-        }
+        // for(int i = 0; i < numerosAleatorios.length; i++) {
+        //     int numero = random.nextInt(100);
+        //     numerosAleatorios[i] = numero;
+        // }
 
-        System.out.print("Numeros Aleatorios: ");
-        for (int numero : numerosAleatorios) {
-            System.out.print(numero + " ");
-        }
+        // System.out.print("Numeros Aleatorios: ");
+        // for (int numero : numerosAleatorios) {
+        //     System.out.print(numero + " ");
+        // }
 
-        System.out.print("\nAntecessor dos Numeros Aleatorios: ");
-        for (int numero : numerosAleatorios) {
-            System.out.print((numero-1) + " ");
-        }
+        // System.out.print("\nAntecessor dos Numeros Aleatorios: ");
+        // for (int numero : numerosAleatorios) {
+        //     System.out.print((numero-1) + " ");
+        // }
 
-        System.out.print("\nSucessores dos Numeros Aleatorios: ");
-        for (int numero : numerosAleatorios) {
-            System.out.print((numero+1) + " ");
-        }
-
-
-
-
+        // System.out.print("\nSucessores dos Numeros Aleatorios: ");
+        // for (int numero : numerosAleatorios) {
+        //     System.out.print((numero+1) + " ");
+        // }
+        int num = 5, count = 1;
+        do {
+            num += count;
+            System.out.println(num);
+        } while (count <= 3);
     }
 
 }
